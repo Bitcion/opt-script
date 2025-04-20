f56c773ed7fc54060fda153cfd31e7e00bf5f451
Bitcion <bitcoinismine@gmail.com>
2025-04-20 17:31:43 +0000

    md5.md5

--- .github/workflows/package_script.sh
+++ .github/workflows/package_script.sh
@@ -8,3 +8,12 @@ echo "开始打包..."
 tar --exclude=".github" -czf opt-script.tgz --transform 's,^,opt-script/,' *
 
 echo "打包完成。"
+
+echo "生成 MD5 校验列表..."
+
+# 生成 MD5 校验列表并保存到 md5.md5 文件，仅包含根目录文件以及 script 和 www_sh 两个文件夹中的文件
+find . -maxdepth 1 -type f ! -name "md5.md5" ! -name "opt-script.tgz" -exec md5sum {} \; > md5.md5
+find ./script -type f -exec md5sum {} \; >> md5.md5
+find ./www_sh -type f -exec md5sum {} \; >> md5.md5
+
+echo "MD5 校验列表已生成，保存在 md5.md5 文件中。"
