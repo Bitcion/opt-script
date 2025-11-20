5c3f0e4f33d83afaef728c88391aa3a8261eadd3
Bitcion <bitcoinismine@gmail.com>
2025-11-20 22:36:49 +0800

    Update Sh99_ss_tproxy.sh

--- script/Sh99_ss_tproxy.sh
+++ script/Sh99_ss_tproxy.sh
@@ -410,7 +410,6 @@ pre_start() {
 post_start() {
     echo "ss-tproxy 启动后执行脚本"
 	  sh /etc/storage/script/ad.sh
-	  nvram set cloudflare_status=123 && /tmp/script/_cloudflare 
     
 }
 pre_stop() {
