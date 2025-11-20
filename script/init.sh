4a8787a84906612f50f24763f577f641b5574d62
Bitcion <bitcoinismine@gmail.com>
2025-11-20 23:39:42 +0800

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -563,3 +563,4 @@ rm -f /tmp/arNslookup/$$
 restart_on_dhcpd() {
 eval "sed \"/""$(cat /tmp/syslog.log | grep -Eo "dnsmasq\[[0-9]+\]: started" | grep -Eo "[0-9]+" | awk '{print "\\\["$1"\\\]";}'  | tr -d "\n" | sed -e "s#\]\\\#\]|\\\#g")""/d\" -Ei /tmp/syslog.log ; restart_dhcpd"
 }
+
